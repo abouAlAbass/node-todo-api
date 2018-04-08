681f74ab4139d717bffce1966851d5d3129abf8f
Wassim <f.wassim@spherewebservices.com>
2018-04-08 16:25:11 +0200

    add the validators

--- server/server.js
+++ server/server.js
@@ -1,43 +1,72 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect("mongodb://localhost:27017/TodoApp");
 
-//save new something 
+//save new something
 
-var Todo = mongoose.model('Todo', {
-    text : {
-        type: String
-    } ,
-    completed:{
-        type :Boolean
-    },
-    completedAt:{
-        type : Number
-    }
+var Todo = mongoose.model("Todo", {
+  text: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  completedAt: {
+    type: Number,
+    default: null
+  }
 });
 
-
 var newTodo = new Todo({
-text : 'cook lunch',
-completed:false
-}); 
+  text: "cook lunch",
+  completed: false
+});
 var otherTodo = new Todo({
-    text : 'eat the lunch',
-    completed:false
-    }); 
+  text: "  Some thing "
+});
 
-newTodo.save().then((doc) => {
+/* newTodo.save().then((doc) => {
 console.log('====================================');
 console.log('Save new Todo',doc);
 console.log('====================================');
 }, (e) => {
     consol.log('unable to save todo')
+});*/
+otherTodo.save().then(
+  doc => {
+    console.log("====================================");
+    console.log("Save other Todo", doc);
+    console.log("====================================");
+  },
+  e => {
+    consol.log("unable to save todo", e);
+  }
+);
+
+var User = mongoose.model("User", {
+  email: {
+    type: String,
+    required: true,
+    minlength: 5,
+    trim: true
+  }
 });
-otherTodo.save().then((doc) => {
-    console.log('====================================');
-    console.log('Save other Todo',doc);
-    console.log('====================================');
-    }, (e) => {
-        consol.log('unable to save todo')
-    });
+
+var newUser = new User({ email: "admin@admin.com" });
+newUser.save().then(
+  doc => {
+    console.log("====================================");
+    console.log("new user", doc);
+    console.log("====================================");
+  },
+  e => {
+    console.log("====================================");
+    console.log("Error", e);
+    console.log("====================================");
+  }
+);
