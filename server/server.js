ba41fd2b610606941b9a4d4dfcc7cf8e2c1fecfa
Wassim <f.wassim@spherewebservices.com>
2018-04-11 09:34:00 +0200

    addAdd GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser =  require('body-parser');
-
+const {ObjectID} = require('mongodb');
 var{mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -28,6 +28,33 @@ app.get('/todos', (req, res) => {
     res.status(400).send(e);
   });
 });
+
+// Get /todos/1245789
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  // valid id using isValid
+  if(!ObjectID.isValid(id)){
+    return   res.status(404).send({error:'Id is not valid'});
+  }
+
+  //findbyID
+  Todo.findById(id).then((todo) => {
+    if(!todo){
+        return res.status(404).send({
+          error : 'theris no todo with this id',
+          id});
+        
+    }
+    return res.status(200).send({todo});
+},(e) => {
+    return res.status(400).send({});
+});  
+    
+ 
+});
+
 
 
 app.listen(3000, () => {
